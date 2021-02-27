73ee3b53b2aa09e5bd56f0ede36b9ab60050a951
MaSkA6293 <MaSkA6293@yandex.ru>
2021-02-27 15:46:47 +0300

    refactor: delet button menu, change button fullscreen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./styles.scss";
-import { ReactComponent as Menu } from "../../assets/menu-button-of-three-horizontal-lines.svg";
 import { ReactComponent as FullScreen } from "../../assets/switch-to-full-screen-button.svg";
 
 export default function Header({ handle }: any): React.ReactElement {
@@ -8,20 +7,12 @@ export default function Header({ handle }: any): React.ReactElement {
     <header className="header">
       <div className="contaner">
         <h1 className="header__title">Quick count</h1>
-        <button className="header__menu" title="Menu">
-          Menu
-          <Menu
-            className="header__menu-image"
-            fill="black"
-            width="20px"
-            height="20px"
-          />
-        </button>
         <button
           className="header__full-screen"
           title="Full screen"
           onClick={handle.enter}
         >
+          <h3 className="header__button-title">Full screen</h3>
           <FullScreen
             className="header__full-screen-image"
             fill="black"
