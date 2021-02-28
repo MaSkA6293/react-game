4efe14fb06dee96a5ab1c5ca6bc5b2befac9d983
MaSkA6293 <MaSkA6293@yandex.ru>
2021-02-28 10:58:57 +0300

    feat: add errorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,30 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
 import { useAudio } from "./hooks/useAudio";
 import mainMusic from "./assets/mainMusic.mp3";
+import ErrorBoundary from "./components/ErrorBoundary";
 function App(): React.ReactElement {
   const handle = useFullScreenHandle();
   const myroot = React.useRef(null);
   const [playing, toggle] = useAudio(mainMusic);
   return (
-    <FullScreen handle={handle}>
-      <Router>
-        <div className="app" ref={myroot}>
-          <Header handle={handle} />
-          <Switch>
-            <Route path="/" exact>
-              <Main playing={playing} toggle={toggle} myroot={myroot} />
-            </Route>
-            <Route path="/game">
-              <Game myroot={myroot} />
-            </Route>
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
-    </FullScreen>
+    <ErrorBoundary>
+      <FullScreen handle={handle}>
+        <Router>
+          <div className="app" ref={myroot}>
+            <Header handle={handle} />
+            <Switch>
+              <Route path="/" exact>
+                <Main playing={playing} toggle={toggle} myroot={myroot} />
+              </Route>
+              <Route path="/game">
+                <Game myroot={myroot} />
+              </Route>
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
+      </FullScreen>
+    </ErrorBoundary>
   );
 }
 
