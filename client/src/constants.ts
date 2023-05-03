ba42c8f459c5b342051a49b640992bc10b4bc6c2
Oleg Shkalikov <maska6293@yandex.ru>
2023-05-03 10:54:02 +0300

    refactor: replace files

--- client/src/constants.ts
+++
@@ -1,7 +0,0 @@
-export enum GameActionsType {
-  START_GAME = 'game/START_GAME',
-  SET_SIGN = 'game/SET_SIGN',
-  SET_COUNT = 'game/SET_COUNT',
-  SET_BEST_RESULTS = 'game/SET_BEST_RESULTS',
-  SET_LEVEL = 'game/SET_LEVEL',
-}
